2ac1e8a93344c68b1c5df70223ae1d7f759bb367
Andy <andyyswoo@gmail.com>
2018-02-24 19:45:40 -0800

    finish first version DAE, w/o side info

--- DAE/src/DAEModel.py
+++ DAE/src/DAEModel.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 from config import config
 
@@ -11,6 +12,7 @@ class DAEModel(object):
         self.build_autoencoder_op()
         self.add_loss_op()
         self.add_optimizer_op()
+        self.add_RMSE_op()
 
     def initialize(self):
         self.sess = tf.Session()
@@ -19,47 +21,123 @@ class DAEModel(object):
         self.sess.run(init)
 
     def add_placeholders_op(self):
-        self.input_placeholder = tf.placeholder(tf.float32)
-        # TODO do this in TF or NP?
-        self.known_placeholder = tf.placeholder(tf.float32) # K
-        self.mask_placeholder = tf.placeholder(tf.float32)  # M
+        self.input_placeholder = tf.placeholder(tf.float32, (None, self.N)) # TODO use metadata
+        self.known_placeholder = tf.placeholder(tf.float32, (None, self.N)) # K
+        self.mask_placeholder = tf.placeholder(tf.float32, (None, self.N))  # M
 
     def build_autoencoder_op(self):
         scope = 'autoencoder'
-        out = self.input_placeholder * (1 - self.mask_placeholder)
+        out = tf.multiply(self.input_placeholder, (1 - self.mask_placeholder))
         with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
             for layer in range(config.n_layers):
-                out = tf.contrib.layers.fully_connected(out, self.config.layer_size, activation_fn=self.config.activation,
-                                                        weights_regularizer=tf.contrib.layers.l2_regularizer,
-                                                        reuse=tf.AUTO_REUSE, scope=scope + '-l['+str(layer) +']')
-        out = tf.contrib.layers.fully_connected(out, self.N, activation_fn=tf.nn.sigmoid,
-                                                        weights_regularizer=tf.contrib.layers.l2_regularizer,
-                                                        reuse=tf.AUTO_REUSE, scope=scope + '-l[out]')
+                out = tf.contrib.layers.fully_connected(out, self.config.layer_size, activation_fn=tf.nn.tanh,
+                                                        weights_regularizer=tf.contrib.layers.l2_regularizer(self.config.lambd),
+                                                        reuse=tf.AUTO_REUSE, scope=scope + '-l-'+str(layer))
+        out = tf.contrib.layers.fully_connected(out, self.N, activation_fn=tf.nn.tanh,
+                                                        weights_regularizer=tf.contrib.layers.l2_regularizer(self.config.lambd),
+                                                        reuse=tf.AUTO_REUSE, scope=scope + '-l-out')
         self.prediction = out
 
     def add_loss_op(self):
-        self.loss = self.config.alpha * tf.losses.mean_squared_error(self.mask_placeholder*self.input_placeholder
-                                                 , self.mask_placeholder*self.prediction) \
-                    + self.config.beta * tf.losses.mean_squared_error((self.known_placeholder - self.mask_placeholder) * self.input_placeholder,
-                                                                      (self.known_placeholder - self.mask_placeholder) * self.prediction)
+        self.loss = self.config.alpha * tf.losses.mean_squared_error(tf.multiply(self.mask_placeholder, self.input_placeholder)
+                                                 , tf.multiply(self.mask_placeholder, self.prediction)) \
+                    + self.config.beta * tf.losses.mean_squared_error(tf.multiply(self.known_placeholder - self.mask_placeholder, self.input_placeholder),
+                                                                      tf.multiply(self.known_placeholder - self.mask_placeholder, self.prediction))
+
+    def add_RMSE_op(self):
+        self.rmse =  tf.sqrt(tf.reduce_mean(tf.squared_difference(tf.multiply(self.known_placeholder, self.input_placeholder),
+                                                                  tf.multiply(self.known_placeholder, self.prediction))))
 
     def add_optimizer_op(self):
         self.train_op = tf.train.AdamOptimizer(learning_rate=self.config.lr).minimize(self.loss)
 
-    def train(self):
-        pass
+    def train(self, R_train, K_train, M_train, R_dev, K_dev, M_dev):
+        data_size = R_train.shape[0]
+        indices = np.arange(data_size)
+        num_batches_per_epoch = 1 + data_size / self.config.batch_size
+        prog = tf.keras.utils.Progbar(target=num_batches_per_epoch * self.config.num_epochs)
+        print 'Initial Dev RMSE: {}'.format(self.evaluate_RMSE(R_dev, K_dev, M_dev))
+        for epoch in range(self.config.num_epochs):
+            if self.config.shuffle:
+                np.random.shuffle(indices)
+            for minibatch_start in np.arange(0, data_size, self.config.batch_size):
+                minibatch_indices = indices[minibatch_start:minibatch_start + self.config.batch_size]
+                R_batch = R_train[minibatch_indices]
+                K_batch = K_train[minibatch_indices]
+                M_batch = M_train[minibatch_indices]
+                # training
+                _, loss = self.sess.run([self.train_op, self.loss], feed_dict={
+                    self.input_placeholder: R_batch,
+                    self.known_placeholder: K_batch,
+                    self.mask_placeholder : M_batch
+                })
+                prog.update(epoch * num_batches_per_epoch+1+minibatch_start/self.config.batch_size,
+                            [('train loss', loss)])
+            # eval on dev set
+            dev_rmse = self.evaluate_RMSE(R_dev, K_dev, M_dev)
+            print '\nepoch: {}, Dev RMSE: {}'.format(epoch+1, dev_rmse)
 
-    def evaluate(self):
-        pass
+    def evaluate_RMSE(self, R, K, M):
+        rmse = self.sess.run([self.rmse], feed_dict={
+            self.input_placeholder: R,
+            self.known_placeholder: K,
+            self.mask_placeholder: np.zeros(M.shape)  # no mask when calculating RMSE
+        })
+        return rmse
+
+    def load_data(self, path):
+        # TODO return metadata too
+        return pd.read_csv(path, delimiter=' ', header=None).as_matrix()
+
+    def random_data(self):
+        N = 6000 # 20133
+        M = 4000 # 6672
+        density = 0.13
+        R = np.random.uniform(1, 5, (M, N)) * ((np.random.uniform(0, 1, (M,N)) < density).astype(int))
+        return R
+
+    def get_known_indices(self, R):
+        return (R != 0).astype(int)
+
+    def get_mask_indices(self, K):
+        return (K * np.random.uniform(0, 1, K.shape) > (1-self.config.mask_ratio)).astype(int)
+
+    # [1, 5] -> [-1, 1], -1.5 -> 0
+    def normalize(self, R):
+        R -= 3
+        R /= 2.0
+        R[R==-1.5] = 0
 
     def run(self):
-        # TODO load data & set N, M accordingly
+        R = self.load_data('../data/ratings')
+        # R = self.random_data()
+        self.N = R.shape[1] # number of users
+        self.M = R.shape[0] # number of movies
 
-        self.N = self.config.N
-        self.M = self.config.M
+        K = self.get_known_indices(R)
+        M = self.get_mask_indices(K)
+        self.normalize(R)
+
+        if self.config.debug:
+            print '# of Users: {}, # of Movies: {}'.format(self.N, self.M)
+            k_c = np.count_nonzero(K)
+            m_c = np.count_nonzero(M)
+            print 'Density: {}, Known: {}, Mask: {}, Mask Ratio: {}'.format(float(k_c)/(self.N * self.M), k_c, m_c, float(m_c) / k_c)
+
+        train_size = int(self.M * 0.8)
+        dev_size = int(self.M * 0.1)
+
+        R_train, R_dev, R_test = np.split(R, [train_size, train_size + dev_size])
+        K_train, K_dev, K_test = np.split(K, [train_size, train_size + dev_size])
+        M_train, M_dev, M_test = np.split(M, [train_size, train_size + dev_size])
+        self.M = R_train.shape[0]
+
         self.build()
         self.initialize()
-        self.train()
+        self.train(R_train, K_train, M_train, R_dev, K_dev, M_dev)
+
+        test_rmse = self.evaluate_RMSE(R_test, K_test, M_test)
+        print 'RMSE on test set: {}'.format(test_rmse)
 
 
 if __name__=='__main__':
--- DAE/src/config.py
+++ DAE/src/config.py
@@ -1,18 +1,24 @@
 import tensorflow as tf
 
 class config():
-    # dataset TODO set this dynamically to model not in config
-    N = 20000   # number of users
-    M = 40000   # number of movies
+    debug = True
+
+    # dataset
+    mask_ratio = 0.25
 
     # metadata
     use_metadata = False
 
     # architecture hyperparameters
-    n_layers = 1                # number of hidden layers
-    layer_size = 750            # number of nuerons per layer
-    activation = tf.nn.relu     # hidden layers' activation
+    n_layers    = 1                 # number of hidden layers
+    layer_size  = 1500               # number of nuerons per layer
+    activation  = tf.nn.relu        # hidden layers' activation
 
-    lr = 1e-3
-    alpha = 1
-    beta  = 0.5
+    # training hyperparameters
+    num_epochs  = 100
+    batch_size  = 500
+    lr          = 1e-3
+    lambd       = 0.0
+    alpha       = 1
+    beta        = 0.5
+    shuffle     = True
